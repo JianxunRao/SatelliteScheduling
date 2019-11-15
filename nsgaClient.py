a39448d57ac11c23b84cc4f7d030e6151f973907
unknown <gapi.petelin@gmail.com>
2019-11-15 10:57:33 +0100

    nsgaClient bugfix

--- nsgaClient.py
+++ nsgaClient.py
@@ -1,32 +1,54 @@
 from platypus import Problem, Integer, NSGAII
 import json
 import requests
+from datetime import datetime
+import sys
+
+# Parameters to be configured (dependant on the problem)
+minSC = 1 # Minimum number of satellite
+maxSC = 10 # Maximum number of satellite
+minGS = 1 # Minimum number of ground station
+maxGS = 5 # Maximum number of ground station
+
+
+maxlLen = 500 # Maximum length of the schedule
+maxTime = 15000 # Maximum time allowed time
+
+populationSize = int(sys.argv[2])
+numberOfEvaluations = int(sys.argv[1])
+
+problemType = 'small'
+problem = 'I_S_01.xml'
+
+now = datetime.now()
 
 def my_function(x):
-    """ Some objective function"""
     jsonDict = dict()
-    jsonDict['problem'] = './small/I_S_01.xml'
+    jsonDict['problem'] = './' + problemType + '/' + problem
     jsonDict['schedule'] = []
     for i in range(x[0]):
-        entry = {'SC': x[i + 1], 'GS': x[i + 2], 'tStart': x[i + 3], 'tDur': x[i + 4]}
+        entry = {'SC': x[4*i + 1], 'GS': x[4*i + 2], 'tStart': x[4*i + 3], 'tDur': x[4*i + 4]}
+        #print(i, entry)
         jsonDict['schedule'].append(entry)
 
     #print(json.dumps(jsonDict))
 
     r = requests.post("http://localhost:8008", data=json.dumps(jsonDict))
-    print(r.status_code, r.reason, r.text)
+    #print(r.status_code, r.reason, r.text)
     obj = json.loads(r.text)
+
+    f = open("trace_nsga_small_I_S_01_" + now.strftime("%H_%M_%S") + ".txt", "a+")
+    
+    log = dict()
+    log['variable'] = jsonDict['schedule']
+    log['objective'] = obj
+    f.write(json.dumps(log) + '\n')
+    f.close()
 
     return obj['FitAW'], obj['FitCS'], obj['FitTR'], obj['FitGU']
 
 
-minSC = 1
-maxSC = 5
-minGS = 1
-maxGS = 5
 
-maxlLen = 100
-maxTime = 15000
 
 scheduleLen = Integer(1, maxlLen)
 var = [scheduleLen]
@@ -41,9 +63,28 @@ problem = Problem(len(var), 4)
 problem.types[:] = var
 problem.directions[:] = Problem.MAXIMIZE
 problem.function = my_function
-algorithm = NSGAII(problem)
-algorithm.run(3000)
-print('Final')
+algorithm = NSGAII(problem, population_size=populationSize)
+algorithm.run(numberOfEvaluations)
+
 for x in algorithm.result:
-    print(x)
+
+    log = dict()
+    log['variable'] = []
+
+    glen = var[0].decode(x.variables[0])
+    for i in range(glen):
+        entry = {'SC': var[4*i + 1].decode(x.variables[4*i + 1]), 'GS': var[4*i + 2].decode(x.variables[4*i + 2]), 
+        'tStart': var[4*i + 3].decode(x.variables[4*i + 3]), 'tDur': var[4*i + 4].decode(x.variables[4*i + 4])}
+        log['variable'].append(entry)
+    
+    obj = dict()
+    obj['FitAW'], obj['FitCS'], obj['FitTR'], obj['FitGU'] = x.objectives[0], x.objectives[1], x.objectives[2], x.objectives[3]
+    log['objective'] = obj
+
+    f = open("nsga_small_I_S_01_" + now.strftime("%H_%M_%S") + ".txt", "a+")
+    
+    
+    f.write(json.dumps(log) + '\n')
+    f.close()
+
 
